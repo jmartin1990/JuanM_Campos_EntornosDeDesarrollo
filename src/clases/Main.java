72083bf892fc5db6ef69eed9b83ba34397713bf8
juan <jmartin.campos90@gmail.com>
2025-04-23 09:06:25 +0200

    JmC_Refactorizado Main con métodos

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -7,9 +7,9 @@ public class Main {
         JmC_Rectangulo rectangulo1 = new JmC_Rectangulo(1, 3.0, 4.0, "rojo");
 
      // Cálculos (versión inicial sin métodos)
-        double areaRect = rectangulo1.getLadoA() * rectangulo1.getLadoB();
-        double areaCirc = circulo1.getRadio() * Math.PI;
-        double diferencia = areaRect - areaCirc;
+        double areaRect = calcularAreaRectangulo(rectangulo1);
+        double areaCirc = calcularAreaCirculo(circulo1);
+        double diferencia = calcularDiferenciaAreas(areaRect, areaCirc);
 
         // Impresión
         System.out.println("Área rectángulo: " + areaRect);
@@ -18,4 +18,19 @@ public class Main {
         System.out.println("Rectángulo: " + rectangulo1);
         System.out.println("Círculo: " + circulo1);
     }
+
+	public static double calcularDiferenciaAreas(double areaRect, double areaCirc) {
+		double diferencia = areaRect - areaCirc;
+		return diferencia;
+	}
+
+	public static double calcularAreaRectangulo(JmC_Rectangulo rectangulo1) {
+		double areaRect = rectangulo1.getLadoA() * rectangulo1.getLadoB();
+		return areaRect;
+	}
+
+	public static double calcularAreaCirculo(JmC_Circulo circulo1) {
+		double areaCirc = circulo1.getRadio() * Math.PI;
+		return areaCirc;
+	}
 }
