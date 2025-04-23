f001e3eae9aafe9e18927f7ad389de9263b24da3
juan <jmartin.campos90@gmail.com>
2025-04-23 09:22:14 +0200

    JmC_Implementado métodos de cálculo e impresión finsl

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -6,31 +6,33 @@ public class Main {
         JmC_Circulo circulo1 = new JmC_Circulo(1, 3.0, "verde");
         JmC_Rectangulo rectangulo1 = new JmC_Rectangulo(1, 3.0, 4.0, "rojo");
 
-     // Cálculos (versión inicial sin métodos)
-        double areaRect = calcularAreaRectangulo(rectangulo1);
-        double areaCirc = calcularAreaCirculo(circulo1);
-        double diferencia = calcularDiferenciaAreas(areaRect, areaCirc);
+        // Cálculos
+        double areaRectangulo = calcularAreaRectangulo(rectangulo1);
+        double areaCirculo = calcularAreaCirculo(circulo1);
+        double diferencia = calcularDiferenciaAreas(areaRectangulo, areaCirculo);
 
-        // Impresión
-        System.out.println("Área rectángulo: " + areaRect);
-        System.out.println("Área círculo: " + areaCirc);
-        System.out.println("Diferencia: " + diferencia);
-        System.out.println("Rectángulo: " + rectangulo1);
-        System.out.println("Círculo: " + circulo1);
+        // Resultados
+        imprimirResultados(rectangulo1, circulo1, diferencia);
     }
 
-	public static double calcularDiferenciaAreas(double areaRect, double areaCirc) {
-		double diferencia = areaRect - areaCirc;
-		return diferencia;
-	}
+    public static double calcularAreaRectangulo(JmC_Rectangulo r) {
+        return r.getLadoA() * r.getLadoB();
+    }
 
-	public static double calcularAreaRectangulo(JmC_Rectangulo rectangulo1) {
-		double areaRect = rectangulo1.getLadoA() * rectangulo1.getLadoB();
-		return areaRect;
-	}
+    public static double calcularAreaCirculo(JmC_Circulo c) {
+        return Math.PI * c.getRadio() * c.getRadio();
+    }
 
-	public static double calcularAreaCirculo(JmC_Circulo circulo1) {
-		double areaCirc = circulo1.getRadio() * Math.PI;
-		return areaCirc;
-	}
+    public static double calcularDiferenciaAreas(double area1, double area2) {
+        return area1 - area2;
+    }
+
+    public static void imprimirResultados(JmC_Rectangulo r, JmC_Circulo c, double dif) {
+        System.out.println("=== RESULTADOS ===");
+        System.out.println("Área del rectángulo (" + r.getColor() + "): " + calcularAreaRectangulo(r));
+        System.out.println("Área del círculo (" + c.getColor() + "): " + calcularAreaCirculo(c));
+        System.out.println("Diferencia de áreas: " + dif);
+        System.out.println("Datos rectángulo: " + r);
+        System.out.println("Datos círculo: " + c);
+    }
 }
